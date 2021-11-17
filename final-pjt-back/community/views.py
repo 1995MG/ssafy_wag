a5fb33b465a89aa691b9f2d63615358092ae128e
SMG <zcvn123456@gmail.com>
2021-11-18 02:02:42 +0900

    article CRUD

--- final-pjt-back/community/views.py
+++ final-pjt-back/community/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 
+from django.shortcuts import get_list_or_404, get_object_or_404
+from .serializers import ArticleSerializer, CommentSerializer
+from .models import Article, Comment
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+@permission_classes([AllowAny])
+def article_list_create(request):
+    if request.method == 'GET':
+        articles = get_list_or_404(Article)
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE', 'GET'])
+@permission_classes([AllowAny])
+def article_detail_update_delete(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+
+    if not request.user.article_set.filter(pk=article_pk).exists():
+        return Response({'detail': '권한이 없습니다.'}, status=status.HTTP_403_FORBIDDON)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+    
+    elif request.method == 'DELETE':
+        article.delete()
+        return Response({ 'id':article_pk }, status=status.HTTP_204_NO_CONTENT)
+    
