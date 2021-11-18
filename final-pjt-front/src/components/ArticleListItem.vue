6b9ca850902ed24fbbca996b6fa066fa858d78f8
SMG <zcvn123456@gmail.com>
2021-11-18 20:18:34 +0900

    update likes

--- final-pjt-front/src/components/ArticleListItem.vue
+++ final-pjt-front/src/components/ArticleListItem.vue
@@ -3,25 +3,54 @@
     <td class="text-center">{{ article.id }}</td>
     <td class="px-5">{{ article.title }}</td>
     <td>{{ article.username }}</td>
-    <td class="text-center">{{ article.created_at }}</td>
-    <td class="text-center">7</td>
+    <td class="text-center">{{ articleCreatedAt }}</td>
+    <td class="text-center">{{ likeCount }}</td>
   </tr>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'ArticleListItem',
+  data: function () {
+    return {
+      likeCount: null,
+      articleCreatedAt: null
+    }
+  },
   methods:{
     onClickRedirect: function () {   
         this.$router.push({ name: 'ArticleDetail' })   
+    },
+    setToken: function () {
+      const token = localStorage.getItem('jwt')
+      const config = {
+        Authorization: `JWT ${token}`
+      }
+      return config
+    },
+    getLikes: function () {
+      axios({
+        method: 'get',
+        url: `http://127.0.0.1:8000/community/${this.article.id}/likes/`,
+        headers: this.setToken()
+      })
+        .then((res) => {
+          this.likeCount = res.data.count
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     },
   },
   props: {
     article: Object
   },
-  // created: function () {
-  //   console.log(this.article)
-  // }
+  created: function () {
+    this.getLikes()
+    this.articleCreatedAt = this.article.created_at.substring(0, 10) + ' ' + this.article.created_at.substring(11, 16)
+  }
 }
 </script>
 
