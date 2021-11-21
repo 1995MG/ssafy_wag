1c1113bb8f69bd6620910a4acd89431f53f04eb3
SMG <zcvn123456@gmail.com>
2021-11-21 16:44:49 +0900

    update BACK

--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render
-from rest_framework import serializers
 from rest_framework.response import Response
 from .models import Movie
-# from .serializers import MovieListSerializers, MovieSerializers
+from .serializers import MovieListSerializers, MovieSerializers
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 # Create your views here.
 
-def index(request):
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def index(request, page):
     # movies = get_list_or_404(Movie.objects.order_by(''))
     # https://wayhome25.github.io/django/2017/03/01/django-99-my-first-project-5/ 참조
-    # movies = get_list_or_404(Movie)
-    # serializers = MovieListSerializers(movies, many=True)
-    # return Response(serializers.data)
-    pass
+    movies = get_list_or_404(Movie)
+    serializers = MovieListSerializers(movies, many=True)
+    return Response(serializers.data[(page-1)*5:page*5])
 
-
+@api_view(['GET'])
+@permission_classes([AllowAny])
 def movie_detail(request, movie_pk):
-    #  movie = get_object_or_404(Movie, pk=movie_pk)
-    #  serializer = MovieSerializers(movie)
-    #  return Response(serializer.data)
-    pass
+     movie = get_object_or_404(Movie, pk=movie_pk)
+     serializer = MovieSerializers(movie)
+     return Response(serializer.data)
