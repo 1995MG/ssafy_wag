7695fa0568f0bd718c22aca9185a5ad78370f3b5
sangjin <toramx5@gmail.com>
2021-11-22 22:18:39 +0900

    update recommend

--- final-pjt-front/src/views/movies/Recommend.vue
+++ final-pjt-front/src/views/movies/Recommend.vue
@@ -22,7 +22,19 @@
     </div>
     <!-- 알고리즘기반영화추천 -->
     <div class="my-5">
-      <h3>그루트추천</h3>
+      <h3 class="fw-bold">그루트추천</h3>
+      <div class="d-flex justify-content-around mt-3">
+          <div @click="toDetail(movie)" class="card" style="width: 18%;"
+            v-for="movie in rcmdMovies" :key="movie.id"
+          >
+            <img :src="`https://www.themoviedb.org/t/p/w440_and_h660_face/${movie.poster_path}`" class="card-img-top" alt="Not Found" onerror="this.src='#';" >
+            <div class="card-body">
+              <p class="card-title fw-bold fs-6">{{ movie.title }}</p>
+              <p>★ {{ movie.vote_average }}</p>
+              <p>{{ movie.release_date | dateParse('YYYY-MM-DD') | dateFormat('YYYY') }}</p>
+            </div>
+          </div>
+        </div>
     </div>
   </div>
 </template>
@@ -30,13 +42,17 @@
 <script>
 import axios from 'axios'
 import _ from 'lodash'
+import jwt_decode from 'jwt-decode'
 
 export default {
   name: 'Recommend',
   data: function () {
     return {
       movies: [],
+      rcmdMovies: [],
       sample: null,
+      userId: null,
+      movieId: null,
     }
   },
   methods: {
@@ -45,7 +61,22 @@ export default {
     },
     getRandom: function () {
       this.sample = _.sampleSize(this.movies, 5)
-    }
+    },
+    getRecommend: function () {
+      axios({
+          method: 'get',
+          url: `http://127.0.0.1:8000/movies/recommended/${this.movieId}/`,
+        })
+          .then((res) => {
+            this.rcmdMovies.push(_.flattenDeep(res.data.DirectorMovie))
+            this.rcmdMovies.push(_.flattenDeep(res.data.actorMovie))
+            this.rcmdMovies.push(_.flattenDeep(res.data.genreMovie))
+            this.rcmdMovies = _.sampleSize(_.flattenDeep(this.rcmdMovies), 5)
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+    },
   },
   created: function () {
     axios({
@@ -60,6 +91,22 @@ export default {
           .catch((err) => {
             console.log(err)
           })
+
+    const token = localStorage.getItem('jwt')
+    this.userId = jwt_decode(token).user_id
+    axios({
+      method: 'get',
+        url: `http://127.0.0.1:8000/accounts/${this.userId}/rank/`,
+        // headers: this.getToken()
+      })
+        .then((res) => {
+          this.movieId = res.data.reverse()[0].movie
+          console.log(this.movieId)
+          this.getRecommend()
+        })
+        .catch((err) => {
+          console.log(err)
+        })
   }
 }
 </script>
