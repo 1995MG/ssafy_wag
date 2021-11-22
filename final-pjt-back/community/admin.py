88f0c0cc614f30229e8331fba13e9c556f1ab6f9
sangjin <toramx5@gmail.com>
2021-11-22 16:02:07 +0900

    update admin

--- final-pjt-back/community/admin.py
+++ final-pjt-back/community/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Article, Comment
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'username', 'title', 'content']
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'article', 'user', 'username', 'content']
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment, CommentAdmin)
