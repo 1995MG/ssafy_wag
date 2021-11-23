484458d6a54b8f7949a0691d0a18962b713d7b2b
sangjin <toramx5@gmail.com>
2021-11-23 19:33:19 +0900

    update movie search

--- final-pjt-front/src/views/movies/Search.vue
+++ final-pjt-front/src/views/movies/Search.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    <movie-list :movies="movies"></movie-list>
+  <div class="container my-5">
+    <!-- 검색창 -->
+    <div class="d-flex mb-5">
+      <input class="form-control" @input="newkeyword=$event.target.value" type="text" :value="keyword" @keyup.enter="toSearch(newkeyword)">
+      <button class="btn btn-success mx-3" @click="toSearch(newkeyword)">search</button>
+    </div>
+    <hr class="text-white">
+    <div v-if="movies.length">
+      <h3 class="text-white mx-3">"{{ keyword }}"에 대한 검색 결과입니다.</h3>
+      <movie-list class="mt-5" :movies="movies"></movie-list>
+    </div>
+    <div v-else class="d-flex justify-content-center">
+      <img src="@/assets/cryinggroot.jpg" width="70%" alt="">
+    </div>
   </div>
 </template>
 
@@ -13,11 +25,19 @@ export default {
   data: function () {
     return {
       keyword: null,
-      movies: null,
+      newkeyword: null,
+      movies: [],
     }
   },
   components: {
     MovieList,
+  },
+  methods: {
+    toSearch: function (newkeyword) {
+      if (newkeyword != null) {
+        this.$router.go(this.$router.push({ name: 'Search', params: {keyword: newkeyword} }))
+      }
+    }
   },
   created: function () {
     this.keyword = this.$route.params.keyword
@@ -26,7 +46,7 @@ export default {
       url: `http://127.0.0.1:8000/movies/search/${this.keyword}/`,
     })
       .then((res) => {
-        console.log(this.keyword)
+        // console.log(this.keyword)
         this.movies = res.data
       })
       .catch((err) => {
